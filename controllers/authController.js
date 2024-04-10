23a981b8e9e04f5bc12d9a372d194529f93043ce
Josh <jpgiant@yahoo.com>
2024-04-10 09:42:19 -0400

    usermodel.js -> userModel.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from '../models/usermodel.js';
+import User from '../models/userModel.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
