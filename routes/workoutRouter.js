6ec7f52eccef6f0a8d411218f8a136063dde562b
Josh <jpgiant@yahoo.com>
2024-04-15 08:30:23 -0400

    verifyAuth add to workoutRouter.js

--- routes/workoutRouter.js
+++ routes/workoutRouter.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 import { getWorkout, createWorkout, updateWorkout, deleteWorkout } from '../controllers/workoutController.js';
 
+import verifyAuth from '../middleware/verifyAuth.js';
+
 router.get('/', getWorkout); 
 
-router.post('/', createWorkout);
+router.post('/', verifyAuth, createWorkout);
 
-router.put('/:title', updateWorkout);
+router.put('/:title', verifyAuth, updateWorkout);
 
-router.delete('/:title', deleteWorkout);
+router.delete('/:title', verifyAuth, deleteWorkout);
 
 export default router;
