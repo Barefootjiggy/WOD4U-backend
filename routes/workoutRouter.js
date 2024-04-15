c1bedf6ab766d0a7eb6cf93eec3e9351f782407e
Josh <jpgiant@yahoo.com>
2024-04-15 11:21:28 -0400

    removal of verifyAuth adds

--- routes/workoutRouter.js
+++ routes/workoutRouter.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 import { getWorkout, createWorkout, updateWorkout, deleteWorkout } from '../controllers/workoutController.js';
 
-import verifyAuth from '../middleware/verifyAuth.js';
+// import verifyAuth from '../middleware/verifyAuth.js';
 
 router.get('/', getWorkout); 
 
-router.post('/', verifyAuth, createWorkout);
+router.post('/', createWorkout);
 
-router.put('/:title', verifyAuth, updateWorkout);
+router.put('/:title', updateWorkout);
 
-router.delete('/:title', verifyAuth, deleteWorkout);
+router.delete('/:title', deleteWorkout);
 
 export default router;
